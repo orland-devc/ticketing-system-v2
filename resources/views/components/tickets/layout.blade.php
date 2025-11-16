e4ca860c496423e37cce5e55816b3fb0e1c1a835
orland-devc <orlandsayson30@gmail.com>
2025-11-16 23:54:02 +0800

    feat: Add blades for Ticket model
    - needed blade for subject and categories

--- resources/views/components/tickets/layout.blade.php
+++ resources/views/components/tickets/layout.blade.php
@@ -2,12 +2,12 @@
     <div class="me-10 w-full pb-2 md:w-[220px]">
         <flux:navlist class="hidden md:block mb-6 ">
             <flux:navlist.settings-item icon="tickets" :href="route('tickets.index')" :current="request()->routeIs('tickets.index')" wire:navigate>{{ __('Tickets') }}</flux:navlist.settings-item>
-            <flux:navlist.settings-item icon="tags" :href="route('offices.staffs')" :current="request()->routeIs('offices.staffs')" wire:navigate>{{ __('Subject and Category') }}</flux:navlist.settings-item>
+            <flux:navlist.settings-item icon="tags" :href="route('tickets.subjects')" :current="request()->routeIs('tickets.subjects')" wire:navigate>{{ __('Subject and Category') }}</flux:navlist.settings-item>
         </flux:navlist>
 
         <div class="flex items-center gap-4 md:hidden -mt-2 mx-2">
             <flux:navlist.settings-item icon="tickets" :href="route('tickets.index')" :current="request()->routeIs('tickets.index')" wire:navigate>{{ __('Tickets') }}</flux:navlist.settings-item>
-            <flux:navlist.settings-item icon="tags" :href="route('offices.staffs')" :current="request()->routeIs('offices.staffs')" wire:navigate>{{ __('Subject and Category') }}</flux:navlist.settings-item>
+            <flux:navlist.settings-item icon="tags" :href="route('tickets.subjects')" :current="request()->routeIs('tickets.subjects')" wire:navigate>{{ __('Subject and Category') }}</flux:navlist.settings-item>
         </div>
     </div>
 
--- resources/views/tickets/subject-category.blade.php
+++ resources/views/tickets/subject-category.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<x-layouts.app :title="__('Subjects and Categories')">
+    <div class="relative flex flex-col gap-3">
+        <div class="">
+            <section class="w-full">
+                @include('partials.tickets-heading')
+                <x-tickets.layout>
+                    <livewire:tickets.index />
+                </x-tickets.layout>
+            </section>
+        </div>
+    </div>
+</x-layouts.app>
+
+<style>
+    .scrollbar-hide::-webkit-scrollbar {
+        display: none;
+    }
+    .scrollbar-hide {
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+</style>
